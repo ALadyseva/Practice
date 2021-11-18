182d2395bf513dea7de88686be6cf05682a83182
ALadyseva <ladyseva03@mail.ru>
2021-11-18 14:21:44 +0300

    Шелл - второй этап

--- shell/part_2.c
+++ shell/part_2.c
@@ -1,11 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 struct item {
 	char *data;
 	struct item *next;
 };
+
+void cleaning(struct item **p){
+	struct item *tmp;	
+	while (*p){
+		tmp = *p;
+		*p = (*p)->next;
+		free(tmp->data);
+		free(tmp);
+	}
+	return;
+}	
+
+char **list_to_array(struct item *head){
+	char **arr = NULL;
+	int i = 0;
+	while (head){
+		arr = (char **)realloc(arr, (i+2)*sizeof(char *));
+		arr[i] = (char *)malloc(sizeof(char) * (strlen(head->data)+1));
+		strcpy(arr[i], head->data);
+		i++;
+		head = head->next;
+	}
+	arr[i] = NULL;
+	return arr;
+} 
+
+void del_array(char **arr){
+	if (arr == NULL)
+		return;
+	
+	int i = 0;
+	while (arr[i] != NULL){
+		free(arr[i]);
+		arr[i] = NULL;
+		i++;
+	}
+	free(arr[i]);
+	arr[i] = NULL;
+	free (arr);
+	arr = NULL;
+}
+
+int is_cd(char **arr){
+	int flag;
+	if (strcmp(arr[0], "cd") == 0){
+		if (arr[1] == NULL) {
+			chdir(getenv("HOME"));
+		}
+		else if (arr[2] != NULL){
+			printf("Error: the system cannot find specified path");
+			return 1;
+		}
+		else{
+			flag = chdir(arr[1]);
+			if (flag == -1){
+				perror(arr[1]);
+			}
+		}
+		return 1;
+	}
+	return 0;
+	
+}
+
+void proc_work(struct item **head){
+	char **arr;
+	int pid;
+	arr = list_to_array(*head);
+	
+	if (is_cd(arr))
+		return;
+	
+	if ((pid = fork())==0){
+		execvp(arr[0], arr);
+		perror(arr[0]);
+		cleaning(head);
+		del_array(arr);
+		exit(1);
+	}
+
+	/*killing zombies*/
+	wait(NULL);
+
+	del_array(arr);
+}
+
 
 void outprint (struct item **p)
 {
@@ -21,12 +110,12 @@ void outprint (struct item **p)
 
 int new_buf(char **p,int len)
 {
-	char *buf;
-	buf=malloc(2*len*sizeof(buf));
-	strncpy(buf,*p,len);
-	free (*p);
-	*p=buf;
-	buf=NULL;
+	//char *buf;
+	//buf=malloc(2*len*sizeof(buf));
+	//strncpy(buf,*p,len);
+	//free (*p);
+	*p=realloc(*p, 2*len*sizeof(char));
+	//buf=NULL;
 	return 2*len;
 }
 
@@ -60,18 +149,6 @@ int err(struct item **p, int *word_len)
 	return 0;
 }
 
-void cleaning(struct item **p){
-	struct item *tmp;
-	while (*p){
-		tmp = *p;
-		*p = (*p)->next;
-		printf("%s\n",(*p)->data);
-		free(tmp->data);
-		free(tmp);
-	}
-	return;
-}	
-
 int endbuf (struct item **head, char *buf, int word_len)
 {
 	buf[word_len]='\0';
@@ -83,7 +160,8 @@ int endlist (struct item **head, char *buf, int word_len)
 {
 	buf[word_len]='\0';
 	*head=buf_to_list(*head, buf, word_len);
-	outprint(head);
+	proc_work(head);
+	cleaning(head);
 	printf(">");
 	word_len=0;
 	return 0;
